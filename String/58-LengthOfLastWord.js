470d9827ff40202a7c78021b0e8b4694a98f1d4f
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2023-12-17 23:37:53 +0500

    58-LengthOfLastWord.js

---
+++ String/58-LengthOfLastWord.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let words=s.match(/\S+/g);
+    let length=words[words.length-1].length;
+    return length;
+};
+
+console.log(lengthOfLastWord("asjaks   js  "));
